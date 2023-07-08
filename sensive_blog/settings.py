98cf087a11778998dec16190560ad0c8753aa9de
Anton Ozerov <lamerork@gmail.com>
2023-07-08 12:07:08 +0300

    Добавил статические файлы

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -98,7 +98,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
-STATIC_ROOT = os.getenv("STATIC_ROOT")
+STATIC_ROOT = os.getenv("STATIC_ROOT", '/static/')
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 
